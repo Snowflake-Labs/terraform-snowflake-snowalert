5058eb89581293ab8eb16f8e33097cac8944d27b
Prasanth <72515998+sfc-gh-pkommini@users.noreply.github.com>
2022-02-22 15:36:03 -0800

    Update geff.tf

--- geff.tf
+++ geff.tf
@@ -16,4 +16,10 @@ module "geff_snowalert" {
   # Other config items
   geff_image_version = var.geff_image_version
   data_bucket_arns   = var.data_bucket_arns
+
+  providers = {
+    snowflake.api_integration     = snowflake.api_integration
+    snowflake.storage_integration = snowflake.storage_integration
+    aws                           = aws
+  }
 }
