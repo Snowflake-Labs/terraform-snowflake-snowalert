531c77989fedfa0b023a3d30a2cb98f36eaf920e
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-06-30 11:53:27 -0700

    Add simple example.

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -8,12 +8,8 @@ module "snowalert" {
   snowalert_warehouse_size         = var.snowalert_warehouse_size
   snowflake_integration_user_roles = [var.snowalert_role_name]
 
-  snowalert_warehouse_name = var.snowalert_warehouse_name
-  snowalert_database_name  = var.snowalert_database_name
-  snowalert_user_name      = var.snowalert_user_name
-  snowalert_role_name      = var.snowalert_role_name
-  snowalert_user_email     = var.snowalert_user_email
-  create_tables            = var.create_tables
+  snowalert_user_email = var.snowalert_user_email
+  create_tables        = var.create_tables
 
   handlers          = var.handlers
   slack_secrets_arn = var.slack_secrets_arn
