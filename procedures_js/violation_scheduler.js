53b2e537a56ec87ab6eb8b59228f2fd8cc65fddc
sfc-gh-nlele <nachiket.lele@snowflake.com>
2022-07-18 18:29:20 +0530

    Test tag-based filtering

--- procedures_js/violation_scheduler.js
+++ procedures_js/violation_scheduler.js
@@ -75,8 +75,8 @@ function get_first_regex_group(regex, s) {
 
 return {
   scheduled: exec(FIND_VIEWS)
-    .filter((v) => (find_tags(`$${v.qualified_view_name}`, 'VIOLATION_SCHEDULE')[0] || {}).TAG_NAME === 'VIOLATION_SCHEDULE')
-    .map((v) => ({
+  .filter((v) => ((find_tags(`$${v.qualified_view_name}`, 'VIOLATION_SCHEDULE')[0] !== null) || v.qualified_view_name.contains('VIOLATION_QUERY')   ))
+  .map((v) => ({
       ...v,
       view_definition: get_ddl(v.qualified_view_name),
     }))
