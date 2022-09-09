3f2849cf00ddea32e6e8964eb1df7ba6f336f075
sfc-gh-nlele <nachiket.lele@snowflake.com>
2022-09-09 13:50:19 +0530

    Remove unneeded violations filter

--- procedures_js/alert_scheduler.js
+++ procedures_js/alert_scheduler.js
@@ -75,7 +75,6 @@ function get_first_regex_group(regex, s) {
 
 return {
   scheduled: exec(FIND_VIEWS)
-    .filter((v) => ((find_tags(`$${v.qualified_view_name}`, 'VIOLATION_SCHEDULE')[0] == null) && !v.qualified_view_name.contains('VIOLATION_QUERY')))
     .map((v) => ({
       ...v,
       view_definition: get_ddl(v.qualified_view_name),
