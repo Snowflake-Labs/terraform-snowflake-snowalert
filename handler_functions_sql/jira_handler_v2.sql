df7fa2f4b54b1b710ff324cecabf1b6edda3638d
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-12-14 00:59:03 +0530

    Use beautified JSON string for JIRA issue description.

--- handler_functions_sql/jira_handler_v2.sql
+++ handler_functions_sql/jira_handler_v2.sql
@@ -15,25 +15,27 @@ ${jira_api_function}(
                     'name', COALESCE(payload['issue_type'], '${default_jira_issue_type}')
                 ),
                 'summary', alert['TITLE']::STRING,
-                'description', ${json_beautify_function}(TO_JSON(
-                    OBJECT_CONSTRUCT(
-                        'Alert ID', alert['ID']::STRING,
-                        'Query ID', alert['QUERY_ID']::STRING,
-                        'Query Name', alert['QUERY_NAME']::STRING,
-                        'Environment', alert['ENVIRONMENT']::STRING,
-                        'Sources', alert['SOURCES']::STRING,
-                        'Categories', COALESCE(alert['CATEGORIES']::STRING, '-'),
-                        'Actor', alert['ACTOR']::STRING,
-                        'Object', alert['OBJECT']::STRING,
-                        'Action', alert['ACTION']::STRING,
-                        'Title', alert['TITLE']::STRING,
-                        'Event Time', alert['EVENT_TIME']::STRING,
-                        'Alert Time', alert['ALERT_TIME']::STRING,
-                        'Detector', alert['DETECTOR']::STRING,
-                        'Severity', alert['SEVERITY']::STRING,
-                        'Description', alert['DESCRIPTION']::STRING,
-                        'Event Data', alert['EVENT_DATA']::STRING
-                    ), 4)
+                'description', ${json_beautify_function}(
+                    TO_JSON(
+                        OBJECT_CONSTRUCT(
+                            'Alert ID', alert['ID']::STRING,
+                            'Query ID', alert['QUERY_ID']::STRING,
+                            'Query Name', alert['QUERY_NAME']::STRING,
+                            'Environment', alert['ENVIRONMENT']::STRING,
+                            'Sources', alert['SOURCES']::STRING,
+                            'Categories', COALESCE(alert['CATEGORIES']::STRING, '-'),
+                            'Actor', alert['ACTOR']::STRING,
+                            'Object', alert['OBJECT']::STRING,
+                            'Action', alert['ACTION']::STRING,
+                            'Title', alert['TITLE']::STRING,
+                            'Event Time', alert['EVENT_TIME']::STRING,
+                            'Alert Time', alert['ALERT_TIME']::STRING,
+                            'Detector', alert['DETECTOR']::STRING,
+                            'Severity', alert['SEVERITY']::STRING,
+                            'Description', alert['DESCRIPTION']::STRING,
+                            'Event Data', alert['EVENT_DATA']::STRING
+                        )
+                    ), 4
                 )
             )
         )
