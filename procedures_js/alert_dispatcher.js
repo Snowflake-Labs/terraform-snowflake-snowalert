bfdf83568326e5ade85f5ca7d49fa51845e42a9e
sfc-gh-nlele <nachiket.lele@snowflake.com>
2024-04-29 13:53:19 +0530

    Update alert_dispatcher.js

--- procedures_js/alert_dispatcher.js
+++ procedures_js/alert_dispatcher.js
@@ -90,14 +90,14 @@ return exec(GET_HANDLERS).rows.map((h) => {
     handled,
     $${handler_num},
     CASE
-      WHEN $${alert_time} > TIMEADD(MINUTES, -$${handler_ttl}, CURRENT_TIMESTAMP)
+      WHEN ? < TIMEADD(MINUTES, -$${handler_ttl}, CURRENT_TIMESTAMP)
       THEN OBJECT_CONSTRUCT('success', FALSE, 'details', 'Alert TTL expired.')
       ELSE $${handler_name}(PARSE_JSON(?), PARSE_JSON(?))
     END
   )
   WHERE alert_id=?
   `,
-  [alert, payload, alert_id]
+  [alert_time, alert, payload, alert_id]
 );
 
 
