122e4b31c13c02a4170d1dcdae402d2d7fb99f2a
Bora Kou <bora.kou@snowflake.com>
2024-05-21 09:36:18 -0700

    Set SUSPEND_TASK_AFTER_NUM_FAILURES parameter to zero on results schema.

--- schemas.tf
+++ schemas.tf
@@ -38,6 +38,20 @@ resource "snowflake_schema" "results" {
   name     = local.results_schema_name
 }
 
+resource "snowflake_object_parameter" "results_schema_suspend_task_after_num_failures" {
+  count    = var.create_schemas == true ? 1 : 0
+  provider = snowflake.admin_role
+
+  key         = "SUSPEND_TASK_AFTER_NUM_FAILURES"
+  value       = "0"
+  object_type = "SCHEMA"
+
+  object_identifier {
+    database = local.snowalert_database_name
+    name     = local.results_schema
+  }
+}
+
 locals {
   results_schema = var.create_schemas == true ? snowflake_schema.results[0].name : local.results_schema_name
 }
